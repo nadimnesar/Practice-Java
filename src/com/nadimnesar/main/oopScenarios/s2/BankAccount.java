4ff4a5368e455944082c1d68fede4c69a500af28
Nesar Ahmed <nadimnesar.cse@gmail.com>
2024-04-17 23:13:42 +0600

    encapsulation s2

---
+++ src/com/nadimnesar/main/oopScenarios/s2/BankAccount.java
@@ -0,0 +1,31 @@
+package com.nadimnesar.main.oopScenarios.s2;
+
+public class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("$" + amount + " deposited successfully.");
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("$" + amount + " withdrawn successfully.");
+        } else {
+            System.out.println("Insufficient funds or invalid withdrawal amount.");
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
--- src/com/nadimnesar/main/oopScenarios/s2/Main.java
+++ src/com/nadimnesar/main/oopScenarios/s2/Main.java
@@ -2,6 +2,10 @@ package com.nadimnesar.main.oopScenarios.s2;
 
 public class Main {
     public static void main(String[] args) {
-
+        BankAccount account = new BankAccount(1000);
+        account.deposit(500);
+        account.withdraw(200);
+        account.withdraw(1500);
+        System.out.println("Current balance: $" + account.getBalance());
     }
 }
